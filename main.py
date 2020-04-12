e4fad4258ed7665ba73be44a3d7a3aa798daf6e9
Danila <danila.komarov@gmail.com>
2020-04-12 14:10:31 +0300

    Edited main run

--- main.py
+++ main.py
@@ -183,4 +183,5 @@ def edit_realty(id):
 if __name__ == "__main__":
     db_session.global_init("db/users.sqlite")
     session = db_session.create_session()
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
