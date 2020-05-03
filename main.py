61f18d9b8a6c871f1bba1b5ac6013374110ed6a5
Danila <danila.komarov@gmail.com>
2020-05-03 10:30:42 +0400

    Main run

--- main.py
+++ main.py
@@ -253,4 +253,4 @@ if __name__ == "__main__":
     db_session.global_init("db/users.sqlite")
     session = db_session.create_session()
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='127.0.0.1', port=port)
+    app.run(host='0.0.0.0', port=port)
